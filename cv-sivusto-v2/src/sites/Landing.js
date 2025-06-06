dbad92eb47ce7960d110923c9800e641a3aed789
AkseliRaj <akseli.rajahalme@gmail.com>
2025-06-06 16:17:54 +0300

    Made landing page responsive

--- cv-sivusto-v2/src/sites/Landing.js
+++ cv-sivusto-v2/src/sites/Landing.js
@@ -7,9 +7,11 @@ function Landing() {
 
     return (
         <div className="landing-background d-flex flex-column justify-content-center align-items-center text-white text-center vh-100">
-            <h1 className="display-1 fw-bold">Hei,</h1>
+            <h1 className="landing-heading px-3 px-md-5">
+                Hei,<span className="d-inline d-sm-none"><br /></span> olen Akseli
+            </h1>
             <button
-                className="gradient-text-button mt-4"
+                className="gradient-text-button mt-3 mt-lg-5 px-4 px-md-5"
                 onClick={() => navigate('/home')}
             >
                 <span className="visually-hidden">Siirry sivustolle</span>
