c2c02b890ba333efd8fb221b09a741a31e36d4ed
AkseliRaj <akseli.rajahalme@gmail.com>
2025-06-06 16:35:27 +0300

    Added global CSS that determines font family, background color and font color the rest of the site

--- cv-sivusto-v2/src/index.js
+++ cv-sivusto-v2/src/index.js
@@ -4,6 +4,8 @@ import { BrowserRouter } from "react-router-dom";
 import App from "./App";
 import "bootstrap/dist/css/bootstrap.min.css";
 import "./index.css";
+import './CSS/global.css';
+
 
 const root = ReactDOM.createRoot(document.getElementById("root"));
 root.render(
