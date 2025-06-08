e5ee074286c6df79167d4c23cf54cdf9b392ebf8
AkseliRaj <akseli.rajahalme@gmail.com>
2025-06-08 13:28:22 +0300

    Added useful skills section to home page

--- cv-sivusto-v2/src/sites/Home.js
+++ cv-sivusto-v2/src/sites/Home.js
@@ -1,10 +1,50 @@
 import React from 'react';
+import '../CSS/Home.css';
+import reactIcon from '../assets/icons/react.svg';
+import jsIcon from '../assets/icons/js.svg';
+import wpIcon from '../assets/icons/wordpress.svg';
+import psIcon from '../assets/icons/photoshop.svg';
+import prIcon from '../assets/icons/premiere.svg';
 
 function Home() {
   return (
-    <div className="text-center">
-      <h1>Etusivu</h1>
-      <p>Esittely itsestäsi</p>
+    <div className="home-wrapper container mt-5">
+
+      <div className="header-section">
+        <h1 className="header-name">
+          Akseli Rajahalme <span className="asterisk">*</span>
+        </h1>
+        <p className="fst-italic subtext">
+          Web developer / UI, UX Designer
+        </p>
+      </div>
+
+      <div className="skills-section mt-5">
+        <p className="subtext mb-4">Pätevyydet alan tehtäviin:</p>
+        <div className="skills-grid">
+          <div className="skill-badge">
+            <img src={reactIcon} alt="React" />
+            <span>React</span>
+          </div>
+          <div className="skill-badge">
+            <img src={wpIcon} alt="Wordpress" />
+            <span>Wordpress</span>
+          </div>
+          <div className="skill-badge">
+            <img src={jsIcon} alt="Javascript" />
+            <span>Javascript</span>
+          </div>
+          <div className="skill-badge">
+            <img src={psIcon} alt="Photoshop" />
+            <span>Photoshop</span>
+          </div>
+          <div className="skill-badge">
+            <img src={prIcon} alt="Premiere Pro" />
+            <span>Premiere Pro</span>
+          </div>
+        </div>
+      </div>
+
     </div>
   );
 }
