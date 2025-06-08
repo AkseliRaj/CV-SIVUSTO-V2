71945f0a99f645aa59f584e82e1cabf64a7c3f41
AkseliRaj <akseli.rajahalme@gmail.com>
2025-06-08 19:43:33 +0300

    Landing transition animation

--- cv-sivusto-v2/src/sites/Landing.js
+++ cv-sivusto-v2/src/sites/Landing.js
@@ -1,23 +1,31 @@
-import React from 'react';
+import React, { useState } from 'react';
 import { useNavigate } from 'react-router-dom';
 import '../CSS/Landing.css';
 
 function Landing() {
-    const navigate = useNavigate();
+  const navigate = useNavigate();
+  const [isFading, setIsFading] = useState(false);
 
-    return (
-        <div className="landing-background d-flex flex-column justify-content-center align-items-center text-white text-center vh-100">
-            <h1 className="landing-heading px-3 px-md-5">
-                Hei,<span className="d-inline d-sm-none"><br /></span> olen Akseli
-            </h1>
-            <button
-                className="gradient-text-button mt-3 mt-lg-5 px-4 px-md-5"
-                onClick={() => navigate('/home')}
-            >
-                <span className="visually-hidden">Siirry sivustolle</span>
-            </button>
-        </div>
-    );
+  const handleTransition = () => {
+    setIsFading(true);
+    setTimeout(() => {
+      navigate('/home');
+    }, 1000); // Odotetaan 1s ennen siirtymistä
+  };
+
+  return (
+    <div className={`landing-background d-flex flex-column justify-content-center align-items-center text-white text-center vh-100 ${isFading ? 'fade-out' : ''}`}>
+      <h1 className={`landing-heading px-3 px-md-5 ${isFading ? 'fade' : ''}`}>
+        Hei,<span className="d-inline d-sm-none"><br /></span> olen Akseli
+      </h1>
+      <button
+        className={`gradient-text-button mt-3 mt-lg-5 px-4 px-md-5 ${isFading ? 'fade' : ''}`}
+        onClick={handleTransition}
+      >
+        <span className="visually-hidden">Siirry sivustolle</span>
+      </button>
+    </div>
+  );
 }
 
 export default Landing;
