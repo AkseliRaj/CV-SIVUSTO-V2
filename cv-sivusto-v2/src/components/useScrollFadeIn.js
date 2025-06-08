7fde76d46b39e848e9327a1f111c95b3cdda5f1e
AkseliRaj <akseli.rajahalme@gmail.com>
2025-06-08 20:06:13 +0300

    Added transition animations for the components in home screen

---
+++ cv-sivusto-v2/src/components/useScrollFadeIn.js
@@ -0,0 +1,25 @@
+import { useEffect, useRef, useState } from 'react';
+
+export default function useScrollFadeIn() {
+  const [isVisible, setVisible] = useState(false);
+  const domRef = useRef();
+
+  useEffect(() => {
+    const observer = new IntersectionObserver(entries => {
+      entries.forEach(entry => {
+        if (entry.isIntersecting) {
+          setVisible(true);
+          observer.unobserve(entry.target); // käynnistyy vain kerran
+        }
+      });
+    });
+
+    if (domRef.current) {
+      observer.observe(domRef.current);
+    }
+
+    return () => observer.disconnect();
+  }, []);
+
+  return [domRef, isVisible];
+}
--- cv-sivusto-v2/src/sites/Home.js
+++ cv-sivusto-v2/src/sites/Home.js
@@ -3,6 +3,7 @@ import '../CSS/Home.css';
 
 import HighlightSection from '../components/HighlightSection.js';
 import ContactForm from '../components/contactForm';
+import useScrollFadeIn from '../components/useScrollFadeIn.js';
 
 import reactIcon from '../assets/icons/react.svg';
 import jsIcon from '../assets/icons/js.svg';
@@ -12,10 +13,19 @@ import prIcon from '../assets/icons/premiere.svg';
 
 
 function Home() {
+
+  const [headerRef, headerVisible] = useScrollFadeIn();
+  const [skillsRef, skillsVisible] = useScrollFadeIn();
+  const [highlightRef, highlightVisible] = useScrollFadeIn();
+  const [contactRef, contactVisible] = useScrollFadeIn();
+
   return (
     <div className="home-wrapper container mt-5">
 
-      <div className="header-section">
+      <div
+        ref={headerRef}
+        className={`header-section fade-in-section ${headerVisible ? 'visible' : ''}`}
+      >
         <h1 className="header-name">
           Akseli Rajahalme <span className="asterisk">*</span>
         </h1>
@@ -24,7 +34,10 @@ function Home() {
         </p>
       </div>
 
-      <div className="skills-section">
+      <div
+        ref={skillsRef}
+        className={`skills-section fade-in-section ${skillsVisible ? 'visible' : ''}`}
+      >
         <p className="subtext mb-4">Pätevyydet alan tehtäviin:</p>
         <div className="skills-grid">
           <div className="skill-badge">
@@ -50,11 +63,15 @@ function Home() {
         </div>
       </div>
 
-      <div>
+      <div ref={highlightRef} className={`fade-in-section ${highlightVisible ? 'visible' : ''}`}>
         <HighlightSection />
       </div>
 
-      <div id="contact" className='contactForm'>
+      <div
+        ref={contactRef}
+        id="contact"
+        className={`contactForm fade-in-section ${contactVisible ? 'visible' : ''}`}
+      >
         <h2 className="header-name">Yhteydenotot</h2>
         <p className="subtext mb-4">akseli.rajahalme@gmail.com<br />+358 44 993 7101</p>
         <ContactForm />
