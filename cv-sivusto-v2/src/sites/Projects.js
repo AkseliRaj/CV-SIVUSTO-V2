1ea4df78a6dfe8b848965a7b38309b0f776c3b9a
AkseliRaj <akseli.rajahalme@gmail.com>
2025-06-12 16:13:09 +0300

    Added header to projects site

--- cv-sivusto-v2/src/sites/Projects.js
+++ cv-sivusto-v2/src/sites/Projects.js
@@ -1,10 +1,20 @@
 import React from 'react';
+import '../CSS/Projects.css'
 
 function Projects() {
   return (
-    <div className="text-center">
-      <h1>Projektit</h1>
-      <p>Tässä näkyy projektisi</p>
+    <div className="Projects-wrapper container mt-5">
+      <div className='header-section'>
+        <h1 className='header-name'>Portfolio</h1>
+        <p className='subtext'>
+          Olen juuri valmistunut Oulun ammattikorkeakousta <a href="https://oamk.fi/koulutus/ammattikorkeakoulututkinnot/tradenomi-amk-tietojenkasittely/" target="_blank">tietojenkäsittelyn tradenomi tutkinnolla.</a> Minulla on kertynyt
+          monipuolisesti kokemusta erilaisista projekteista kuten verkkosivustojen,
+          videopelien  sekä mobiilisovellusten kehittämisestä. Alhaalla näkyy muutamia ryhmä- sekä yksilö
+          projekteja, missä olen ollut mukana. Löydät kaikki ohjelmointi projektini <a href="https://github.com/AkseliRaj" target="_blank"> Github profiilistani.</a>
+        </p>
+      </div>
+
+
     </div>
   );
 }
